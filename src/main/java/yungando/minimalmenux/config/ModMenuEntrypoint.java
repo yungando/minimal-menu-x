2f0aea3a8efc6c69eeccf785c4b00ad7c96dcb14
James Anderson <yung@ando.ws>
2026-09-08 22:15:22 +0100

    Send null config screen when cloth config is not installed

--- src/main/java/yungando/minimalmenux/config/ModMenuEntrypoint.java
+++ src/main/java/yungando/minimalmenux/config/ModMenuEntrypoint.java
@@ -3,11 +3,14 @@ package yungando.minimalmenux.config;
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
 import me.shedaniel.autoconfig.AutoConfigClient;
+import net.fabricmc.loader.api.FabricLoader;
 import yungando.minimalmenux.config.MinimalMenuXConfig.MinimalMenuXAutoConfig;
 
 public class ModMenuEntrypoint implements ModMenuApi {
   @Override
   public ConfigScreenFactory<?> getModConfigScreenFactory() {
-    return parent -> AutoConfigClient.getConfigScreen(MinimalMenuXAutoConfig.class, parent).get();
+    return FabricLoader.getInstance().isModLoaded("cloth-config")
+      ? parent -> AutoConfigClient.getConfigScreen(MinimalMenuXAutoConfig.class, parent).get()
+      : ModMenuApi.super.getModConfigScreenFactory();
   }
 }
